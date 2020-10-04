b97764991a03488ebd8e372975ca9426eed34b9d
jmhdevelop <44244434+jmhdevelop@users.noreply.github.com>
2020-10-04 13:31:50 +0200

    winter is commit

---
+++ Resistance/ResistanceTests/ResistanceTests.swift
@@ -0,0 +1,33 @@
+//
+//  ResistanceTests.swift
+//  ResistanceTests
+//
+//  Created by José Miguel Herrero on 4/10/20.
+//
+
+import XCTest
+@testable import Resistance
+
+class ResistanceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
